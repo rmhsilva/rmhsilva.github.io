d3f628cc22bab6b8cdf3b44a22fde90497412674
Ric da Silva <2575243+rmhsilva@users.noreply.github.com>
2019-12-18 09:15:04 +0000

    Remove ansible from bs
    Not used anymore.

--- bs.sh
+++ bs.sh
@@ -2,7 +2,7 @@
 #
 # curl -fsSL https://rmhsilva.com/bs.sh | /bin/bash
 #
-# Installs git, python, and dotfiles
+# Installs git, python, stow, and dotfiles
 
 if [ "$EUID" -eq 0 ]; then
     echo Not meant to be run as root!
@@ -19,20 +19,13 @@ if [ "$(uname)" = "Darwin" ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew install git
     brew install python
+    brew install stow
 fi
-
-pip3 install ansible
 
 # checkout dotfiles
 cd $HOME
 git clone -o origin_https https://github.com/rmhsilva/dotfiles/ .dotfiles
 cd .dotfiles
 git submodule update --init --recursive
-
-# create initial Ansible inventory
-cat <<EOF > ~/.dotfiles/inventory
-[local]
-localhost  ansible_connection=local
-EOF
 
 echo Dotfiles checked out to ~/.dotfiles
